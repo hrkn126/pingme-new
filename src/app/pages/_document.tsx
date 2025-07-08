df5009bb9633de42b35edca2eba4d386b50a7f3a
はるね <harune@harunenoMacBook-Air.local>
2025-07-08 18:07:08 +0900

    fix: fix circle layout overflow bug

--- src/app/pages/_document.tsx
+++ src/app/pages/_document.tsx
@@ -1,10 +1,16 @@
-// pages/_document.tsx
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="ja" translate="no">
-      <Head />
+      <Head>
+        {/* ✅ AdSense のコード */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1576343547879212"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
